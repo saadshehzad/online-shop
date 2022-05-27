da456e3e6bd482c253e8ef5e1eb897803adac42f
Muhammad Shehzad <shehzad@broadstonetech.com>
2022-05-27 16:36:52 +0500

    Add product detail url

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from  .views import product_list, product_detail
 
+from .views import product_detail, product_list
 
 urlpatterns = [
-    path("", product_list, name="product_list")
+    path("", product_list, name="product_list"),
+    path("<slug:category_slug>", product_list, name="product_list_by_category"),
+    path("<int:id>/<slug:slug>/", product_detail, name="product_detail"),
 ]
