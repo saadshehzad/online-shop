de3ee9233e1b8e06e379da74790fb59523a52a91
Muhammad Shehzad <shehzad@broadstonetech.com>
2022-05-27 16:32:01 +0500

    Add product detail view

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,3 @@
-from itertools import product
-from unicodedata import category
-
 from django.shortcuts import get_object_or_404, render
 
 from .models import Category, Product
@@ -18,3 +15,8 @@ def product_list(request, category_slug):
         "shop/product/list.html",
         {"category": category, "categories": categories, "products": products},
     )
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, "shop/product/detail.html", {"product": product})
